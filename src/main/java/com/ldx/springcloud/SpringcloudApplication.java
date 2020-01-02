d57460633e390870e942c635c4bbaaf1e48def0e
ldx <853764876@qq.com>
2020-01-02 17:44:41 +0800

    测试修改提交

--- src/main/java/com/ldx/springcloud/SpringcloudApplication.java
+++ src/main/java/com/ldx/springcloud/SpringcloudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcloudApplication {
 
     public static void main(String[] args) {
-
+		String a = "";
         SpringApplication.run(SpringcloudApplication.class, args);
     }
 
